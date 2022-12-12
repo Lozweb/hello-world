1e62aaaa7c654fb16d0148c0037ea164e774c258
hermelen <hermelen.peris@gmail.com>
2022-12-12 14:14:13 +0100

    add hw cli

--- back/src/main.rs
+++ back/src/main.rs
@@ -6,12 +6,11 @@ async fn main() {
         .allow_origins(vec!["http://localhost:3000", "http://localhost"])
         .allow_methods(vec!["GET", "POST", "DELETE"]);
     
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
     let hello = warp::path!("hello" / String)
         .map(|name| format!("Hello, {}!", name))
         .with(cors);
-
+    
     warp::serve(hello)
-        .run(([127, 0, 0, 1], 3030))
+        .run(([0, 0, 0, 0], 3030))
         .await;
 }
